dc8a5e12da7fa1f5292bbe6ab5bfe349c9e540a6
Saduakas-kbtu-2026 <s_salimgerei@kbtu.kz>
2026-02-05 18:29:34 +0500

    Create boolean_intro.py

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,20 @@
+x = "Hello"
+y = 15
+
+print(bool(x)) #True
+print(bool(y)) #True
+
+print(bool("abc"))                          #True
+print(bool(123))                            #True
+print(bool(["apple", "cherry", "banana"]))  #True
+
+#"empty" values are false, for example:
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({}) 
+# is all False

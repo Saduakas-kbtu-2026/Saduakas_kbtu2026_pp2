3cbd67ff456ca640c91311a38638e4ff5948c1de
Saduakas-kbtu-2026 <s_salimgerei@kbtu.kz>
2026-02-05 18:27:38 +0500

    Create casting.py

---
+++ Practice1/python-basics/casting.py
@@ -0,0 +1,8 @@
+#ints can be converted to floats and strings
+x = 1
+y = 2.8
+z = "4"
+
+print(int(x), int(y), int(z))       # 1     | 2     | 4
+print(float(x), float(y), float(z)) # 1.0   | 2.8   | 4.0
+print(str(x), str(y), str(z))       # 1     | 2.8   | 4

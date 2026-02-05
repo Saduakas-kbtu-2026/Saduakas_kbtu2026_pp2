a4b556680701390a2f65ec97b54cd6941ae464f0
Saduakas-kbtu-2026 <s_salimgerei@kbtu.kz>
2026-02-05 18:30:32 +0500

    Create boolean_comparisons.py

---
+++ Practice2/boolean/boolean_comparisons.py
@@ -0,0 +1,11 @@
+print(10 > 9) #true
+print(10 == 9)#false
+print(10 < 9) #false
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")     #false
+else:
+  print("b is not greater than a") #true

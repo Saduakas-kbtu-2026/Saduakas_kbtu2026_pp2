8bd7c174096a0d874a27ba1cfc09a7fe8be0bb5f
Saduakas-kbtu-2026 <s_salimgerei@kbtu.kz>
2026-02-11 23:36:05 +0500

    Update basic_function.py

--- Practice3/functions/basic_function.py
+++ Practice3/functions/basic_function.py
@@ -3,17 +3,5 @@ def my_function():
 
 my_function()
 
-def fahrenheit_to_celsius(fahrenheit): # helps avoid repetition
-  return (fahrenheit - 32) * 5 / 9
-
-print(fahrenheit_to_celsius(77))
-print(fahrenheit_to_celsius(95))
-print(fahrenheit_to_celsius(50)) 
-
-def get_greeting():
-  return "Hello from a function"
-
-message = get_greeting()
-print(message) 
-#can also be written as
-print(get_greeting())
+for i in range(30):
+    my_function()

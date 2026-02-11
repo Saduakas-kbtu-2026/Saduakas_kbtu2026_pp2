3d337ef6ebfed7620eb3d2c3834045d27e01b337
Saduakas-kbtu-2026 <s_salimgerei@kbtu.kz>
2026-02-11 23:36:39 +0500

    Update basic_function.py

--- Practice3/functions/basic_function.py
+++ Practice3/functions/basic_function.py
@@ -5,3 +5,11 @@ my_function()
 
 for i in range(30):
     my_function()
+
+def get_greeting():
+  return "Hello from a function"
+
+message = get_greeting()
+print(message) 
+#can also be written as
+print(get_greeting())
